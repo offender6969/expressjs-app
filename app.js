54b66cec09d8298614635ae4281014f738c87f2c
offender <98567050+offender6969@users.noreply.github.com>
2023-11-02 23:07:16 +0530

    update port no
    as it was clashing with grafana

--- app.js
+++ app.js
@@ -6,6 +6,6 @@ app.get("/", (req, res) => {
     res.send("<h2>HELLO WORLD</h2>");
 });
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
