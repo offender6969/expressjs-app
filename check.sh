44e8f57c568594164b37962c7dac92a371dc3d69
offender <98567050+offender6969@users.noreply.github.com>
2023-11-02 22:25:52 +0530

    Create check.sh

---
+++ check.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Define the name of the container
+CONTAINER_NAME="express-js"
+
+# Check if the container exists
+if [[ $(docker ps -q -f name="$CONTAINER_NAME") ]]; then
+  echo "Container $CONTAINER_NAME exists. Stopping and removing..."
+  # Stop the container
+  docker stop "$CONTAINER_NAME"
+  # Remove the container
+  docker rm "$CONTAINER_NAME"
+else
+  echo "Container $CONTAINER_NAME does not exist."
+fi
+
+
+
+
+
+
+
+
